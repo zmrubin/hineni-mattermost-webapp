ec655aa08c62946783fa803929a3e0653c35c049
JMGB <jesus82c@gmail.com>
2023-03-14 00:43:28 -0400

    Update header_footer_template.tsx

--- components/header_footer_template/header_footer_template.tsx
+++ components/header_footer_template/header_footer_template.tsx
@@ -1,11 +1,11 @@
-// Copyright (c) 2015-present Mattermost, Inc. All Rights Reserved.
+// Copyright (c) 2015-present most, Inc. All Rights Reserved.
 // See LICENSE.txt for license information.
 
 import PropTypes from 'prop-types';
 import React from 'react';
 import {FormattedMessage} from 'react-intl';
 
-import {ClientConfig} from '@mattermost/types/config';
+import {ClientConfig} from '@/types/config';
 
 type Props = {
     config: Partial<ClientConfig> | undefined;
@@ -112,7 +112,7 @@ export default class NotLoggedIn extends React.PureComponent<Props> {
                 <div className='row content'>
                     {this.props.children}
                 </div>
-                <div className='row footer'>
+                <div className='row footer' style="display:none">
                     <div
                         id='footer_section'
                         className='footer-pane col-xs-12'
@@ -120,9 +120,9 @@ export default class NotLoggedIn extends React.PureComponent<Props> {
                         <div className='col-xs-12'>
                             <span
                                 id='company_name'
-                                className='pull-right footer-site-name'
+                                className='pull-right footer-site-name' 
                             >
-                                {'Mattermost'}
+                                {'AI Ship'}
                             </span>
                         </div>
                         <div className='col-xs-12'>
@@ -130,7 +130,7 @@ export default class NotLoggedIn extends React.PureComponent<Props> {
                                 id='copyright'
                                 className='pull-right footer-link copyright'
                             >
-                                {`© 2015-${new Date().getFullYear()} Mattermost, Inc.`}
+                                {`© 2023 AI Ship`}
                             </span>
                             <span className='pull-right'>
                                 {content}
